541139e08c6fcaa85385a9054bc42352d93d5d1a
samuelu112 <urb23434@uvg.edu.gt>
2025-04-28 09:02:45 -0600

    Laboratorio6
    Laboratorio y postlaboratorio

---
+++ Postlaboratorio6/Postlaboratorio6/main.c
@@ -0,0 +1,18 @@
+/*
+ * Postlaboratorio6.c
+ *
+ * Created: 28/04/2025 09:01:15
+ * Author : samur
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
